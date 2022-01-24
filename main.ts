3552a55810b4c77cd30b7ee3beab925405e9784e
Shaun McDonald <shaun@shaunmcdonald.me.uk>
2022-01-24 11:57:55 +0000

    Stop after the loop, otherwise it just keeps going and crashes.

--- main.ts
+++ main.ts
@@ -1,14 +1,15 @@
 input.onButtonPressed(Button.A, function () {
     gigglebot.servoMove(gigglebotServoAction.Left, 90)
-    for (let index = 0; index < 50; index++) {
+    for (let index = 0; index < 100; index++) {
         if (gigglebot.distanceSensorTestForObstacle(gigglebotInequality.Closer, 100)) {
             gigglebot.stop()
         } else if (gigglebot.distanceSensorTestForObstacle(gigglebotInequality.Closer, 300)) {
-            gigglebot.spinMillisec(gigglebotWhichTurnDirection.Right, 100)
+            gigglebot.spinMillisec(gigglebotWhichTurnDirection.Right, 200)
         } else {
             gigglebot.driveMillisec(gigglebotWhichDriveDirection.Forward, 400)
         }
     }
+    gigglebot.stop()
 })
 input.onButtonPressed(Button.B, function () {
     gigglebot.stop()
